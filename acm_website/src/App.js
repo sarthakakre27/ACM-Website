3b1aaa0216dab1d909038cab5a4cb221457d88e8
meetagrawal09 <agrawalmeet91@gmail.com>
2022-01-02 19:13:23 +0530

    problem-list

--- acm_website/src/App.js
+++ acm_website/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import './App.css';
 
+import Cover from './Components/Cover';
+
+import ProblemDetails from './Components/ProblemDetails';
+
+import ProblemList from './Components/ProblemList';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+	return (
+		<Router>   
+			<div className="app">
+				<Switch>
+					<Route path="/problem-list">
+						<ProblemList/>
+					</Route>
+					<Route path="/problem-details">
+						<ProblemDetails/>
+					</Route>
+					<Route path="/">
+						<Cover/>
+					</Route>
+				</Switch>
+			</div>
+  		</Router>
+	);
 }
 
 export default App;
